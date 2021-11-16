a1dd8712aa06d44f9f9f6596c7d8584df4caf593
John Bryan Valle <jbvalle.jv@gmail.com>
2021-11-16 14:02:35 +0100

    removed flag with local index variable

--- coding/prog/seach_easyprob/main.c
+++ coding/prog/seach_easyprob/main.c
@@ -4,29 +4,33 @@
 
 int main(void){
 
-    int num_people, flag = 0;
+    int num_people;
     char diff[1000];
     char letter;
 
     scanf("%d", &num_people);getchar();
 
     //Initializing State input
-    for(int i = 0; i < 1000;i++)diff[i] = '\n';
+    for(int i = 0; i < 1000;i++)diff[i] = '\0';
 
     for(int i = 0; (letter = getchar()) != '\n';i++)
     {
        diff[i] = letter;
     }
 
-    for(size_t j = 0; j < strlen(diff);j++){
+    for(int j = 0; j < strlen(diff);j++){
 
         if(diff[j] == '1'){
             printf("HARD");
             flag = 1;
             break;
         }
+        
+        if((j == (strlen(diff)-1)) && (diff[j] == '0')){
+        
+            printf("EASY");
+        }
     }
 
-    if(flag == 0)printf("EASY");
     return 0;
 }
